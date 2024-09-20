c3f25b404493ee9584571546848a1fdfb31dbd29
hzkhora <kaplogheinzschai@gmail.com>
2024-09-20 08:14:46 +0800

    changes in client/pages/index.vue

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -146,7 +146,7 @@
                         </fieldset>
                     </div>
                     <div
-                        class="isolate mx-auto mt-10 grid max-w-md grid-cols-1 gap-8 md:max-w-2xl md:grid-cols-2 lg:max-w-4xl xl:mx-0 xl:max-w-none xl:grid-cols-4">
+                        class="isolate mx-auto mt-10 grid max-w-md grid-cols-1 gap-8 md:max-w-2xl md:grid-cols-2 lg:max-w-4xl xl:mx-0 xl:max-w-none xl:grid-cols-3">
                         <div v-for="tier in pricing.tiers" :key="tier.id"
                             :class="[tier.mostPopular ? 'ring-2 ring-indigo-600' : 'ring-1 ring-gray-200', 'rounded-3xl p-8']">
                             <h2 :id="tier.id"
@@ -169,6 +169,151 @@
                                 </li>
                             </ul>
                         </div>
+                    </div>
+                </div>
+                <div class="relative lg:pt-14">
+                    <div class="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8">
+                        <!-- Feature comparison (up to lg) -->
+                        <section aria-labelledby="mobile-comparison-heading" class="lg:hidden">
+                            <h2 id="mobile-comparison-heading" class="sr-only">Feature comparison</h2>
+
+                            <div class="mx-auto max-w-2xl space-y-16">
+                                <div v-for="tier in pricing.tiers" :key="tier.id" class="border-t border-gray-900/10">
+                                    <div
+                                        :class="[tier.featured ? 'border-indigo-600' : 'border-transparent', '-mt-px w-72 border-t-2 pt-10 md:w-80']">
+                                        <h3
+                                            :class="[tier.featured ? 'text-indigo-600' : 'text-gray-900', 'text-sm font-semibold leading-6']">
+                                            {{ tier.name }}</h3>
+                                        <p class="mt-1 text-sm leading-6 text-gray-600">{{ tier.description }}</p>
+                                    </div>
+
+                                    <div class="mt-10 space-y-10">
+                                        <div v-for="section in pricing.sections" :key="section.name">
+                                            <h4 class="text-sm font-semibold leading-6 text-gray-900">{{ section.name }}
+                                            </h4>
+                                            <div class="relative mt-6">
+                                                <!-- Fake card background -->
+                                                <div aria-hidden="true"
+                                                    class="absolute inset-y-0 right-0 hidden w-1/2 rounded-lg bg-white shadow-sm sm:block" />
+
+                                                <div
+                                                    :class="[tier.featured ? 'ring-2 ring-indigo-600' : 'ring-1 ring-gray-900/10', 'relative rounded-lg bg-white shadow-sm sm:rounded-none sm:bg-transparent sm:shadow-none sm:ring-0']">
+                                                    <dl class="divide-y divide-gray-200 text-sm leading-6">
+                                                        <div v-for="feature in section.features" :key="feature.name"
+                                                            class="flex items-center justify-between px-4 py-3 sm:grid sm:grid-cols-2 sm:px-0">
+                                                            <dt class="pr-4 text-gray-600">{{ feature.name }}</dt>
+                                                            <dd
+                                                                class="flex items-center justify-end sm:justify-center sm:px-4">
+                                                                <span
+                                                                    v-if="typeof feature.tiers[tier.name] === 'string'"
+                                                                    :class="tier.featured ? 'font-semibold text-indigo-600' : 'text-gray-900'">{{
+                                                                        feature.tiers[tier.name] }}</span>
+                                                                <template v-else>
+                                                                    <CheckIcon v-if="feature.tiers[tier.name] === true"
+                                                                        class="mx-auto h-5 w-5 text-indigo-600"
+                                                                        aria-hidden="true" />
+                                                                    <XMarkIconMini v-else
+                                                                        class="mx-auto h-5 w-5 text-gray-400"
+                                                                        aria-hidden="true" />
+                                                                    <span class="sr-only">{{ feature.tiers[tier.name]
+                                                                        === true ? 'Yes' : 'No' }}</span>
+                                                                </template>
+                                                            </dd>
+                                                        </div>
+                                                    </dl>
+                                                </div>
+
+                                                <!-- Fake card border -->
+                                                <div aria-hidden="true"
+                                                    :class="[tier.featured ? 'ring-2 ring-indigo-600' : 'ring-1 ring-gray-900/10', 'pointer-events-none absolute inset-y-0 right-0 hidden w-1/2 rounded-lg sm:block']" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+
+                        <!-- Feature comparison (lg+) -->
+                        <section aria-labelledby="comparison-heading" class="hidden lg:block">
+                            <h2 id="comparison-heading" class="sr-only">Feature comparison</h2>
+
+                            <div class="grid grid-cols-4 gap-x-8 border-t border-gray-900/10 before:block">
+                                <div v-for="tier in pricing.tiers" :key="tier.id" aria-hidden="true" class="-mt-px">
+                                    <div
+                                        :class="[tier.featured ? 'border-indigo-600' : 'border-transparent', 'border-t-2 pt-10']">
+                                        <p
+                                            :class="[tier.featured ? 'text-indigo-600' : 'text-gray-900', 'text-sm font-semibold leading-6']">
+                                            {{
+                                                tier.name }}</p>
+                                        <p class="mt-1 text-sm leading-6 text-gray-600">{{ tier.description }}</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="-mt-6 space-y-16">
+                                <div v-for="section in pricing.sections" :key="section.name">
+                                    <h3 class="text-sm font-semibold leading-6 text-gray-900">{{ section.name }}</h3>
+                                    <div class="relative -mx-8 mt-10">
+                                        <!-- Fake card backgrounds -->
+                                        <div class="absolute inset-x-8 inset-y-0 grid grid-cols-4 gap-x-8 before:block"
+                                            aria-hidden="true">
+                                            <div class="h-full w-full rounded-lg bg-white shadow-sm" />
+                                            <div class="h-full w-full rounded-lg bg-white shadow-sm" />
+                                            <div class="h-full w-full rounded-lg bg-white shadow-sm" />
+                                        </div>
+
+                                        <table class="relative w-full border-separate border-spacing-x-8">
+                                            <thead>
+                                                <tr class="text-left">
+                                                    <th scope="col">
+                                                        <span class="sr-only">Feature</span>
+                                                    </th>
+                                                    <th v-for="tier in pricing.tiers" :key="tier.id" scope="col">
+                                                        <span class="sr-only">{{ tier.name }} tier</span>
+                                                    </th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr v-for="(feature, featureIdx) in section.features"
+                                                    :key="feature.name">
+                                                    <th scope="row"
+                                                        class="w-1/4 py-3 pr-4 text-left text-sm font-normal leading-6 text-gray-900">
+                                                        {{ feature.name }}
+                                                        <div v-if="featureIdx !== section.features.length - 1"
+                                                            class="absolute inset-x-8 mt-3 h-px bg-gray-200" />
+                                                    </th>
+                                                    <td v-for="tier in pricing.tiers" :key="tier.id"
+                                                        class="relative w-1/4 px-4 py-0 text-center">
+                                                        <span class="relative h-full w-full py-3">
+                                                            <span v-if="typeof feature.tiers[tier.name] === 'string'"
+                                                                :class="[tier.featured ? 'font-semibold text-indigo-600' : 'text-gray-900', 'text-sm leading-6']">{{
+                                                                    feature.tiers[tier.name] }}</span>
+                                                            <template v-else>
+                                                                <CheckIcon v-if="feature.tiers[tier.name] === true"
+                                                                    class="mx-auto h-5 w-5 text-indigo-600"
+                                                                    aria-hidden="true" />
+                                                                <XMarkIconMini v-else
+                                                                    class="mx-auto h-5 w-5 text-gray-400"
+                                                                    aria-hidden="true" />
+                                                                <span class="sr-only">{{ feature.tiers[tier.name] ===
+                                                                    true ? 'Yes' : 'No' }}</span>
+                                                            </template>
+                                                        </span>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+
+                                        <!-- Fake card borders -->
+                                        <div class="pointer-events-none absolute inset-x-8 inset-y-0 grid grid-cols-4 gap-x-8 before:block"
+                                            aria-hidden="true">
+                                            <div v-for="tier in pricing.tiers" :key="tier.id"
+                                                :class="[tier.featured ? 'ring-2 ring-indigo-600' : 'ring-1 ring-gray-900/10', 'rounded-lg']" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
                     </div>
                 </div>
                 <!-- Logo cloud -->
@@ -197,7 +342,8 @@
                             <span class="hidden md:inline">Transistor saves up to $40,000 per year, per employee by
                                 working with us.</span>
                             <a href="#" class="font-semibold text-indigo-600"><span class="absolute inset-0"
-                                    aria-hidden="true" /> See our case study <span aria-hidden="true">&rarr;</span></a>
+                                    aria-hidden="true" /> See
+                                our case study <span aria-hidden="true">&rarr;</span></a>
                         </p>
                     </div>
                 </div>
@@ -429,58 +575,69 @@ const features = [
 //pricing area functions
 const pricing = {
     frequencies: [
-        { value: 'monthly', label: 'Monthly', priceSuffix: '/month' },
-        { value: 'annually', label: 'Annually', priceSuffix: '/year' },
+        { value: 'monthly', label: 'Monthly' },
+        { value: 'annually', label: 'Annually' },
     ],
     tiers: [
         {
-            name: 'Hobby',
-            id: 'tier-hobby',
+            name: 'Starter',
+            id: 'tier-starter',
             href: '#',
+            featured: false,
+            description: 'All your essential business finances, taken care of.',
             price: { monthly: '$15', annually: '$144' },
-            description: 'The essentials to provide your best work for clients.',
-            features: ['5 products', 'Up to 1,000 subscribers', 'Basic analytics'],
-            mostPopular: false,
+            mainFeatures: ['Basic invoicing', 'Easy to use accounting', 'Mutli-accounts'],
         },
         {
-            name: 'Freelancer',
-            id: 'tier-freelancer',
+            name: 'Scale',
+            id: 'tier-scale',
             href: '#',
-            price: { monthly: '$30', annually: '$288' },
-            description: 'The essentials to provide your best work for clients.',
-            features: ['5 products', 'Up to 1,000 subscribers', 'Basic analytics', '48-hour support response time'],
-            mostPopular: false,
-        },
-        {
-            name: 'Startup',
-            id: 'tier-startup',
-            href: '#',
+            featured: true,
+            description: 'The best financial services for your thriving business.',
             price: { monthly: '$60', annually: '$576' },
-            description: 'A plan that scales with your rapidly growing business.',
-            features: [
-                '25 products',
-                'Up to 10,000 subscribers',
-                'Advanced analytics',
-                '24-hour support response time',
-                'Marketing automations',
+            mainFeatures: [
+                'Advanced invoicing',
+                'Easy to use accounting',
+                'Mutli-accounts',
+                'Tax planning toolkit',
+                'VAT & VATMOSS filing',
+                'Free bank transfers',
             ],
-            mostPopular: true,
         },
         {
-            name: 'Enterprise',
-            id: 'tier-enterprise',
+            name: 'Growth',
+            id: 'tier-growth',
             href: '#',
-            price: { monthly: '$90', annually: '$864' },
-            description: 'Dedicated support and infrastructure for your company.',
+            featured: false,
+            description: 'Convenient features to take your business to the next level.',
+            price: { monthly: '$30', annually: '$288' },
+            mainFeatures: ['Basic invoicing', 'Easy to use accounting', 'Mutli-accounts', 'Tax planning toolkit'],
+        },
+    ],
+    sections: [
+        {
+            name: 'Catered for business',
             features: [
-                'Unlimited products',
-                'Unlimited subscribers',
-                'Advanced analytics',
-                '1-hour, dedicated support response time',
-                'Marketing automations',
-                'Custom reporting tools',
+                { name: 'Tax Savings', tiers: { Starter: true, Scale: true, Growth: true } },
+                { name: 'Easy to use accounting', tiers: { Starter: true, Scale: true, Growth: true } },
+                { name: 'Multi-accounts', tiers: { Starter: '3 accounts', Scale: 'Unlimited accounts', Growth: '7 accounts' } },
+                { name: 'Invoicing', tiers: { Starter: '3 invoices', Scale: 'Unlimited invoices', Growth: '10 invoices' } },
+                { name: 'Exclusive offers', tiers: { Starter: false, Scale: true, Growth: true } },
+                { name: '6 months free advisor', tiers: { Starter: false, Scale: true, Growth: true } },
+                { name: 'Mobile and web access', tiers: { Starter: false, Scale: true, Growth: false } },
             ],
-            mostPopular: false,
+        },
+        {
+            name: 'Other perks',
+            features: [
+                { name: '24/7 customer support', tiers: { Starter: true, Scale: true, Growth: true } },
+                { name: 'Instant notifications', tiers: { Starter: true, Scale: true, Growth: true } },
+                { name: 'Budgeting tools', tiers: { Starter: true, Scale: true, Growth: true } },
+                { name: 'Digital receipts', tiers: { Starter: true, Scale: true, Growth: true } },
+                { name: 'Pots to separate money', tiers: { Starter: false, Scale: true, Growth: true } },
+                { name: 'Free bank transfers', tiers: { Starter: false, Scale: true, Growth: false } },
+                { name: 'Business debit card', tiers: { Starter: false, Scale: true, Growth: false } },
+            ],
         },
     ],
 }
